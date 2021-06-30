62bc31fef0c8e4f0896cbf86fcc815ff3b2c93b1
Gitopham1356 <75571286+Gitopham1356@users.noreply.github.com>
2021-06-30 13:53:19 +0700

    Add project files.

---
+++ 1811061037_PhamDangMinhTriet_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1811061037_PhamDangMinhTriet_BigSchool.Startup))]
+namespace _1811061037_PhamDangMinhTriet_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
